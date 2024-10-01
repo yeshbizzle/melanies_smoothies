83c007042754f0afa3fe9ec2c3644d328820edf8
Yogesh C <48980915+yeshbizzle@users.noreply.github.com>
2024-10-01 19:04:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+
 
 ingredients_list = st.multiselect(
     "choose upto 5 options:",my_dataframe,
